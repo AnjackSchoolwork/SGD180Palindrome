857544801126b236a8e30b781294d9f5cc59cdce
Anjack <gary.reif@gmail.com>
2017-09-12 12:32:45 -0400

    Removed extraneous code.

--- Palindrome.js
+++ Palindrome.js
@@ -5,13 +5,10 @@ function setup() {
 
 function checkPalindrome(textInput) {
 	var userInput = textInput.replace(/[^A-Za-z0-9]/g, "").split(" ").join("").split("")
-	console.log(userInput)
 	
 	var tempArray = userInput
 	userInput = userInput.join("")
 	tempArray.reverse()
-
-	console.log(tempArray)
 	
 	if (tempArray.join("").toLowerCase() == userInput.toLowerCase()) {
 		document.getElementById("answerLabel").innerHTML = "That is a palindrome!"
