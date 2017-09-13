3cc272db912eac03882fd9ba177c72987c1823c0
Anjack <gary.reif@gmail.com>
2017-09-12 21:20:13 -0400

    Added some text effects. Further pretty-fication.

--- Palindrome.js
+++ Palindrome.js
@@ -4,16 +4,26 @@ function setup() {
 }
 
 function checkPalindrome(textInput) {
+
+    var wordDisplayLabel = document.getElementById("wordDisplay")
+    wordDisplayLabel.setAttribute("style", "")
+
+    var answerLabel = document.getElementById("answerLabel")
+    answerLabel.setAttribute("style", "")
+
 	var userInput = textInput.replace(/[^A-Za-z0-9]/g, "").split(" ").join("").split("")
 	
 	var tempArray = userInput
 	userInput = userInput.join("")
 	tempArray.reverse()
-	
-	if (tempArray.join("").toLowerCase() == userInput.toLowerCase()) {
-		document.getElementById("answerLabel").innerHTML = "That is a palindrome!"
+
+    wordDisplayLabel.innerHTML = textInput
+
+    if (tempArray.join("").toLowerCase() == userInput.toLowerCase()) {
+        wordDisplayLabel.setAttribute("style", "animation-name: rotate; animation-duration: 6s; animation-iteration-count: infinite;")
+        answerLabel.innerHTML = "That is a palindrome!"
 	}
 	else {
-		document.getElementById("answerLabel").innerHTML = "That is NOT a palindrome."
+		answerLabel.innerHTML = "That is NOT a palindrome."
 	}
 }
